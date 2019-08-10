8b151baeb20181f03ae3a9c45319f28851af4d6b
wlanboy <wlanboy@github.com>
2019-08-10 15:50:45 +0200

    Added volume mount for docker socket

--- JenkinsStack/jenkins-slave.sh
+++ JenkinsStack/jenkins-slave.sh
@@ -1,1 +1,1 @@
-docker run --name jenkins-slave -d --restart unless-stopped wlanboy/jenkins-docker-openjdk11-slave -url http://nuc:8080 -workDir=/home/jenkins/agent secret javaworker
+docker run --name jenkins-slave -d -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped wlanboy/jenkins-docker-openjdk11-slave -url http://nuc:8080 -workDir=/home/jenkins/agent secret javaworker
