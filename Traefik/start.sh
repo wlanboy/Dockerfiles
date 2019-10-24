1f1cc626dc5f0d07dfd8fe6b015744cd2b89e4a3
wlanboy <wlanboy@github.com>
2019-10-24 10:09:43 +0200

    Added dynamic directory watch for Traefik

--- Traefik/start.sh
+++ Traefik/start.sh
@@ -1,1 +1,2 @@
-docker run --name traefik -p 80:80 -v $PWD/traefik.yml:/etc/traefik/traefik.yml -v $PWD/dynamic_conf.yml:/etc/traefik/dynamic_conf.yml -d --restart unless-stopped traefik:2.0.1
+docker run --name traefik -p 80:80 -p 8083:8080 -v $PWD/traefik.yml:/etc/traefik/traefik.yml -v $PWD/conf:/etc/traefik/conf -d --restart unless-stopped traefik:2.0.1
+
