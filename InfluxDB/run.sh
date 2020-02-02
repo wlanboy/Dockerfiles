3de8a91e95bc800478de96f057a63c149a26dd2b
wlanboy <wlanboy@users.noreply.github.com>
2020-02-02 07:49:02 +0100

    Create run.sh
    Added InfluxDB

---
+++ InfluxDB/run.sh
@@ -0,0 +1,7 @@
+docker run -d --name influx -p 8086:8086 influxdb
+
+#docker exec -it influx influx
+#Connected to http://localhost:8086 
+#InfluxDB shell 
+#$ create database springboot
+#$ use springboot
