3f365abfdfed442f08c9544c5b2356f0bb0b3acb
wlanboy <wlanboy@github.com>
2020-02-16 14:16:59 +0100

    Create start-arm64.sh
    Nextcloud for arm64v8

---
+++ Nextcloud/start-arm64.sh
@@ -0,0 +1,6 @@
+docker run -d \
+    -v nextcloud:/var/www/html \
+    -v apps:/var/www/html/custom_apps \
+    -v config:/var/www/html/config \
+    -v data:/var/www/html/data \
+    arm64v8/nextcloud:18.0.1-fpm-alpine
