bc3edbbb96ae28d868ac359ad45c4ffaa252b953
wlanboy <wlanboy@users.noreply.github.com>
2020-02-16 13:58:04 +0100

    Create start-arm64.sh
    Added arm64

---
+++ Postgres/start-arm64.sh
@@ -0,0 +1,1 @@
+docker run --name postgres -d -p 5432:5432 -v $PWD/postgres.sql:/docker-entrypoint-initdb.d/init.sql -e POSTGRES_PASSWORD=spring -e POSTGRES_USER=spring -e POSTGRES_DB=spring --restart unless-stopped arm64v8/postgres:11.7-alpine
