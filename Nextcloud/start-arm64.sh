8b9509e50be137b9b952ace7cc419ed1a03ffe14
wlanboy <wlanboy@github.com>
2020-04-07 22:37:58 +0200

    Update start-arm64.sh

--- Nextcloud/start-arm64.sh
+++ Nextcloud/start-arm64.sh
@@ -3,4 +3,4 @@ docker run -d -p 8080:80 --name next \
     -v apps:/var/www/html/custom_apps \
     -v config:/var/www/html/config \
     -v data:/var/www/html/data \
-    arm64v8/nextcloud:18.0.1-fpm-alpine 
+    arm64v8/nextcloud:18.0.3-fpm-alpine 
