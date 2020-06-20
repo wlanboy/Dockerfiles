44465cd0c9ca4db22c9277b3ece7ed7fafab3fb1
wlanboy <wlanboy@users.noreply.github.com>
2020-06-20 08:58:40 +0200

    Update elastic-only.sh

--- Elk/elastic-only.sh
+++ Elk/elastic-only.sh
@@ -1,2 +1,1 @@
-
-docker run --name elastic -p 9200:9200 -p 9300:9300 -m 1024M -d -e "discovery.type=single-node" --restart unless-stopped docker.elastic.co/elasticsearch/elasticsearch:7.8.1
+docker run --name elastic -p 9200:9200 -p 9300:9300 -m 1024M -d -e "discovery.type=single-node" --restart unless-stopped docker.elastic.co/elasticsearch/elasticsearch:7.8.0
