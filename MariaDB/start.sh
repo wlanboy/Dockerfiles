047dbc604335471d07ce31c535c2dc5eee5368bf
wlanboy <wlanboy@github.com>
2020-10-11 08:42:32 +0200

    Updated mariadb version

--- MariaDB/start.sh
+++ MariaDB/start.sh
@@ -1,1 +1,1 @@
-docker run --name mysqldb -p 3306:3306 -v /mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=spring -e MYSQL_USER=spring -e MYSQL_PASSWORD=spring -d --restart unless-stopped mariadb:10.3.10
+docker run --name mariadb -p 3306:3306 -v /mariadb:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=spring -e MYSQL_USER=spring -e MYSQL_PASSWORD=spring -d --restart unless-stopped mariadb:10.5
