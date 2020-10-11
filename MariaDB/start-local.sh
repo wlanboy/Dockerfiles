d1bb9c8dc6ece5cf9c3baba711cb6484bfced6c0
wlanboy <wlanboy@users.noreply.github.com>
2020-10-11 08:43:03 +0200

    Update start-local.sh

--- MariaDB/start-local.sh
+++ MariaDB/start-local.sh
@@ -1,1 +1,1 @@
-docker run --name mysqldb -v /mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=spring -e MYSQL_USER=spring -e MYSQL_PASSWORD=spring -d -p 127.0.0.1:3306:3306 --restart unless-stopped mariadb:10.3.10
+docker run --name mariadb -v /mariadb:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=spring -e MYSQL_USER=spring -e MYSQL_PASSWORD=spring -d -p 127.0.0.1:3306:3306 --restart unless-stopped mariadb:10.5
