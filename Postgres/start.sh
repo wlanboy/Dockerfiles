aecf0c416b43ad845358aab1b4ac7b7952fff140
wlanboy <wlanboy@users.noreply.github.com>
2022-12-31 09:07:07 +0100

    Update postgresql version

--- Postgres/start.sh
+++ Postgres/start.sh
@@ -1,1 +1,1 @@
-docker run --name postgres -d -p 5432:5432 -v /postgresdata:/var/lib/postgresql/data -v $PWD/postgres.sql:/docker-entrypoint-initdb.d/init.sql -e POSTGRES_PASSWORD=spring -e POSTGRES_USER=spring -e POSTGRES_DB=spring --restart unless-stopped postgres:12.2-alpine
+docker run --name postgres -d -p 5432:5432 -v /postgresdata:/var/lib/postgresql/data -v $PWD/postgres.sql:/docker-entrypoint-initdb.d/init.sql -e POSTGRES_PASSWORD=spring -e POSTGRES_USER=spring -e POSTGRES_DB=spring --restart unless-stopped postgres:15.1-alpine
