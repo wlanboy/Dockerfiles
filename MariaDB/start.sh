97b7dcbc5a7df82ebe9c10d736de71247d69d30b
wlanboy <wlanboy@github.com>
2024-06-15 21:04:01 +0200

    Update start.sh

--- MariaDB/start.sh
+++ MariaDB/start.sh
@@ -1,1 +1,1 @@
-docker run --name mariadb -p 3306:3306 -v /mariadb:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=spring -e MYSQL_USER=spring -e MYSQL_PASSWORD=spring -d --restart unless-stopped mariadb:11.2
+docker run --name mariadb -p 3306:3306 -v /mariadb:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=spring -e MYSQL_USER=spring -e MYSQL_PASSWORD=spring -d --restart unless-stopped mariadb:11.4
