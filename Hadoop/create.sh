0e6c57b8539f2554d7980155e1462318e988d789
wlanboy <wlanboy@users.noreply.github.com>
2024-09-14 07:48:11 +0200

    Update create.sh

--- Hadoop/create.sh
+++ Hadoop/create.sh
@@ -3,3 +3,5 @@ docker run -d --net hadoop --net-alias yarnmaster --name yarnmaster -h yarnmaste
 docker run -d --net hadoop --net-alias namenode --name namenode -h namenode -p 8020:8020 -p 50070:50070 -p 50090:50090 --restart unless-stopped swapnillinux/cloudera-hadoop-namenode
 docker run -d --net hadoop --net-alias datanode1 --name datanode1 -h datanode1 --link namenode --link yarnmaster swapnillinux/cloudera-hadoop-datanode
 docker exec namenode hdfs dfs -chmod -R 777 /
+docker exec namenode hdfs dfs -mkdir /user1
+docker exec namenode hdfs dfs -mkdir /user2
