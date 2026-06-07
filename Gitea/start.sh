5490fb63e78374413664541e3725d59cec3b24b6
wlanboy <wlanboy@github.com>
2026-06-07 12:34:08 +0200

    Added comments

--- Gitea/start.sh
+++ Gitea/start.sh
@@ -1,1 +1,14 @@
-docker run -d --name gitea --publish 8022:22 --publish 3000:3000 --env DOMAIN=nuclinux --env ROOT_URL=http://nuclinux:3000 --env USER_UID=1000 --env USER_GID=999 --volume /giteadata:/data --volume /etc/timezone:/etc/timezone:ro --volume /etc/localtime:/etc/localtime:ro --restart unless-stopped gitea/gitea:latest
+# Startet Gitea - selbst gehosteter Git-Service.
+# Web-UI erreichbar unter http://gmk.lan:3000
+docker run -d --name gitea \
+    --publish 8022:22 \                          # SSH-Zugriff für git clone/push (Host-Port 8022 → Container 22)
+    --publish 3000:3000 \                        # Web-UI und HTTP-Git-Zugriff
+    --env DOMAIN=gmk.lan \                       # Hostname für generierte Clone-URLs
+    --env ROOT_URL=http://gmk.lan:3000 \         # Vollständige Basis-URL (wichtig für Links in der UI)
+    --env USER_UID=1000 \                        # UID des gitea-Prozesses im Container (Host-User angleichen)
+    --env USER_GID=999 \                         # GID des gitea-Prozesses im Container
+    --volume /giteadata:/data \                  # Persistenter Speicher für Repos, DB und Konfiguration
+    --volume /etc/timezone:/etc/timezone:ro \    # Zeitzone des Hosts in den Container übernehmen
+    --volume /etc/localtime:/etc/localtime:ro \  # Lokale Zeit des Hosts in den Container übernehmen
+    --restart unless-stopped \
+    gitea/gitea:latest
