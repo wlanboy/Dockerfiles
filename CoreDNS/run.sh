f63e4bf95ee92117e984d1081ad1e3b5e7ae344d
wlanboy <wlanboy@github.com>
2026-06-07 12:24:38 +0200

    Added comments

--- CoreDNS/run.sh
+++ CoreDNS/run.sh
@@ -1,1 +1,7 @@
-docker run -d --name coredns --restart=always --volume=$PWD/coredns/:/root/ -p 53:53/udp coredns/coredns -conf /root/Corefile
+# Startet CoreDNS mit dem lokalen coredns/-Verzeichnis als Konfigurationsordner.
+# $PWD macht das Skript ortsunabhängig - Corefile und Zone-Dateien liegen relativ zum Skript.
+# Port 53/udp ist der Standard-DNS-Port.
+docker run -d --name coredns --restart=always \
+    --volume=$PWD/coredns/:/root/ \   # Corefile + Zone-Dateien ins Container-Root einbinden
+    -p 53:53/udp \                    # DNS-Port nach außen freigeben
+    coredns/coredns -conf /root/Corefile
--- CoreDNS/runhost.sh
+++ CoreDNS/runhost.sh
@@ -1,1 +1,7 @@
-docker run -d --name coredns --restart=always --volume=/git/Dockerfiles/CoreDNS/coredns/:/root/ -p 53:53/udp coredns/coredns -conf /root/Corefile
+# Wie run.sh, aber mit absolutem Pfad statt $PWD.
+# Gedacht für den Fall, dass das Skript nicht aus dem Repo-Verzeichnis heraus gestartet wird
+# (z.B. als systemd-Service oder Cron-Job).
+docker run -d --name coredns --restart=always \
+    --volume=/git/Dockerfiles/CoreDNS/coredns/:/root/ \   # absoluter Pfad zur Konfiguration
+    -p 53:53/udp \
+    coredns/coredns -conf /root/Corefile
